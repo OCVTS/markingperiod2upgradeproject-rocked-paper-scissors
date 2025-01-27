325ca75b17084fcb6bc5a2cb797485cad0693c88
Souplessness <37212@ocvts.org>
2025-01-27 01:15:53 +0000

    mostly functional, still work to do

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let computerChoice = '';
 let playerScore = 0;
 let computerScore = 0;
 
+let currentModifier = '';
+let userChoice2 = '';
+
 // let randomizedNumber = 0;
 
 // Object containing all selectable options
@@ -16,35 +19,96 @@ const options = {
     'water': { beats: ['rock', 'paper', 'bomb', 'man'], tiesWith: ['water','scissors'], value: 1, info: '71% of the Earth\'s surface and about 60% of YOU.' },
     'paper towel': { beats: ['rock', 'water'], tiesWith: ['paper towel', 'paper'], value: 1, info: 'Good for cleaning spills and not much else.' },
     'man': { beats: ['paper towel', 'random number'], tiesWith: ['man'], value: 100, info: 'This is Ben. Say hi! (he has a very weak constitution)' },
-    'random number': { beats: ['Technically nothing'], tiesWith: ['Probably itself'], value: numberRandomizer(), info: 'LET\'S GO GAMBLING!' },
+    'random number': { beats: ['Technically nothing'], tiesWith: ['Probably itself'], value: 0, info: 'LET\'S GO GAMBLING!!! (Note: This option forces the Value Mod)' },
     'wind': { beats: [], tiesWith: [], value: 0, info: 'A gust of wind.' },
     'shoot': { beats: ['man'], tiesWith: ['shoot'], value: 0, info: 'You ever play normal Rock Paper Scissors and have someone treat "SHOOT" as something you can actually pick? No? Well now you can!' },
     'gun': { beats: [], tiesWith: [], value: 30, info: 'This is a firearm.' }
 } // 'template': { beats: [], tiesWith: [], value: 0, info: '' }
 
+// Object containing all merge-only options
+const mergedOptions = {
+    'corpse': { beats: [], tiesWith: [], value: 0, info: 'Ben died :(', combos: ['man' + 'bomb'] }
+} // 'template': { beats: [], tiesWith: [], value: 0, info: '', combos: [] }
+
+// Object containing all upgrade-only options
+const upgradedOptions = {
+    'boulder': { beats: [], tiesWith: [], value: 0, info: '', origin: 'rock' }
+} // 'template': { beats: [], tiesWith: [], value: 0, info: '', origin: '' }
+
 // Essentially unmodified from the original. Gets the input from the pressed button, runs getComputerChoice(), and then puts them into determineWinner()
 function getUserChoice(userInput) {
-    userChoice = userInput.toLowerCase();
-    document.getElementById('userChoice').innerHTML = ('Player Selected: ' + formatText(userChoice));
+    userChoice = userInput;
+    document.getElementById('userChoice').innerHTML = ('Player: ' + formatText(userChoice));
 }
 
-// Takes all of the options from the options object and makes an array with them. It then selects a random index of that array, returning a random option (this works no matter how many options are added)
+// Takes all of the options from an options object and makes an array with them. It then selects a random index of that array, returning a random option (this works no matter how many options are added). Has a 10% chance to use an alternate options object
 function getComputerChoice() {
-    choices = Object.keys(options);
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
+    num = Math.floor(Math.random() * 100) + 1;
+    if (num >= 1 && num <= 5) {
+        choices = Object.keys(mergedOptions); // Uses mergedOptions (5%)
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        return choices[randomIndex];
+    } else if (num >= 6 && num <= 10) {
+        choices = Object.keys(upgradedOptions); // Uses upgradedOptions (5%)
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        return choices[randomIndex];
+    } else {
+        choices = Object.keys(options); // Uses options (90%)
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        return choices[randomIndex];
+    }
 }
 
-// Using an object to keep track of all of this was an excellent decision. It not only saved me from having to write a million if statements, but also streamlined adding new options. This function takes the 2 choice variables and plugs them into some simple if statements
-function determineWinner(userChoice) {
-    computerChoice = getComputerChoice();
-    document.getElementById('computerChoice').innerHTML = ('Computer Selected: ' + formatText(computerChoice));
+function setModifier(mod) {
+    currentModifier = mod;
+    document.getElementById('modifier').innerHTML = ('Modifier: ' + formatText(currentModifier));
+}
+
+function modifierCheck(userChoice) {
+    if (userChoice == 'random number') { // Forces Value
+        currentModifier = 'value';
+        document.getElementById('modifier').innerHTML = ('Modifier: ' + formatText(currentModifier));
+    }
+    
+    if (currentModifier == 'value') {
+        determineValueWinner(userChoice);
+    } else {
+        determineWinner(userChoice);
+    }
+}
+
+// Using an object to keep track of all of this was an excellent decision. It not only saved me from having to write a million if statements, but also streamlined adding new options 
+function determineWinner(userChoice) { // This function takes the 2 choice variables and plugs them into some simple if statements
+    computerChoice = getComputerChoice(); // Does computer stuff first
+    document.getElementById('computerChoice').innerHTML = ('Computer: ' + formatText(computerChoice));
+
     if (options[userChoice].tiesWith.includes(computerChoice)) { // Checks if computerChoice is on the list of things that userChoice ties with
         document.getElementById('winner').innerHTML = (`It's a tie...`); 
     } else if (options[userChoice].beats.includes(computerChoice)) { // If there isn't a tie, checks if computerChoice is on the list of things userChoice beats
         playerScore++;
         document.getElementById('winner').innerHTML = ('Player wins!');
     } else if (options[computerChoice].beats.includes(userChoice)) { // If it isn't on that list, checks if userChoice is on the list of things that computerChoice beats
+        computerScore++;
+        document.getElementById('winner').innerHTML = ('Computer wins!');
+    }
+    // Updates score in HTML
+    document.getElementById('computerScore').innerHTML = ('Computer Score: ' + computerScore);
+    document.getElementById('playerScore').innerHTML = ('Player Score: ' + playerScore);
+}
+
+// Identical to the original, except for how it detects who wins. It's far simpler, actually, since it's just dealing with numbers
+function determineValueWinner(userChoice) {
+    options['random number'].value = numberRandomizer(); // Randomizes the value of Random Number so that it isn't what it was when it was hovered
+
+    computerChoice = getComputerChoice();
+    document.getElementById('computerChoice').innerHTML = ('Computer: ' + formatText(computerChoice));
+
+    if (options[userChoice].value === options[computerChoice].value) {
+        document.getElementById('winner').innerHTML = (`It's a tie...`); 
+    } else if (options[userChoice].value > options[computerChoice].value) {
+        playerScore++;
+        document.getElementById('winner').innerHTML = ('Player wins!');
+    } else if (options[userChoice].value < options[computerChoice].value) {
         computerScore++;
         document.getElementById('winner').innerHTML = ('Computer wins!');
     }
@@ -65,7 +129,7 @@ function formatText(input) {
 // This is for the tooltip when you hover a button. I searched a lot to try to get it to work how I wanted, and eventually just asked chatGPT and it gave me what I wanted immediately. I also did a ton of messing with it to make it work exactly how I wanted. I've spent hours on this single feature...
 const buttons = document.querySelectorAll('.optionButton');
 const tooltip = document.getElementById('tooltip');
-const buttonIdP = document.getElementById('buttonId');
+const buttonNameP = document.getElementById('buttonId');
 const buttonValueP = document.getElementById('buttonValue');
 const buttonInfoP = document.getElementById('buttonInfo');
 const buttonBeatsP = document.getElementById('buttonBeats');
@@ -73,15 +137,16 @@ const buttonTiesWithP = document.getElementById('buttonTiesWith');
 
 buttons.forEach(button => {
     // Makes the tooltip div visible and changes the text of it for which button you hover
-    button.addEventListener('mouseover', (e) => {
-        numberRandomizer();
-        const buttonId = e.target.id;
-        const buttonValue = options[e.target.id].value;
-        const buttonInfo = options[e.target.id].info;
-        const buttonBeats = options[e.target.id].beats;
-        const buttonTiesWith = options[e.target.id].tiesWith;
+    button.addEventListener('mouseover', (hover) => {
+        options['random number'].value = numberRandomizer(); // Randomizer runs on each hover
         
-        buttonIdP.innerText = `${formatText(buttonId)}`;
+        const buttonName = hover.target.id;
+        const buttonValue = options[hover.target.id].value;
+        const buttonInfo = options[hover.target.id].info;
+        const buttonBeats = options[hover.target.id].beats;
+        const buttonTiesWith = options[hover.target.id].tiesWith;
+        
+        buttonNameP.innerText = `${formatText(buttonName)}`;
         buttonValueP.innerText = `Value: ${buttonValue}`;
         buttonInfoP.innerText = `${buttonInfo}`;
         buttonBeatsP.innerText = `Beats: ${formatText(buttonBeats)}`;
@@ -92,22 +157,29 @@ buttons.forEach(button => {
     });
 
     // Tracks the position of the tooltip div based on the cursor
-    button.addEventListener('mousemove', (e) => {
-        numberRandomizer();
+    button.addEventListener('mousemove', (pos) => {
         const offsetX = 10; 
         const offsetY = 20;
-        tooltip.style.left = `${e.pageX + offsetX}px`;
-        tooltip.style.top = `${e.pageY + offsetY}px`;
+        tooltip.style.left = `${pos.pageX + offsetX}px`;
+        tooltip.style.top = `${pos.pageY + offsetY}px`;
     });
 
     // Hides the tooltip div again
     button.addEventListener('mouseout', () => {
-        numberRandomizer();
         tooltip.style.display = 'none';
         tooltip.style.opacity = 0;
     });
 });
 
+// Randomizes Random Number
 function numberRandomizer() {
     return Math.floor(Math.random() * 30) + 1;
+}
+
+function getDate() {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    return `${month}/${day}/${year}`;
 }
